c82c4a603b162df33b545590c046d5197660e018
naens <naens@gmx.com>
2020-07-29 10:54:58 +0200

    sql: migrate to postgresql

--- cvd-pg.sh
+++ cvd-pg.sh
@@ -1,8 +1,10 @@
 #! /bin/dash
 
-db_file=cvd-data.db
 web_source="https://covid.ourworldindata.org/data/owid-covid-data.json"
 data_file=tmp/covid-data.json
+pg_user=pgwiz
+pg_db=cvd19
+
 
 day_start="2019-01-01"
 
@@ -26,13 +28,13 @@ day_to_date ()
 
 cvd_db_init ()
 {
-    sqlite3 $db_file << EOF
+    local sql="
     CREATE TABLE IF NOT EXISTS country (
-        id INTEGER PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         key TEXT UNIQUE,
         name TEXT,
         continent TEXT,
-        population INTEGER
+        population BIGINT
     );
 
     CREATE TABLE IF NOT EXISTS data (
@@ -41,8 +43,8 @@ cvd_db_init ()
         new_cases INTEGER,
         PRIMARY KEY(day, country),
         FOREIGN KEY (country) REFERENCES country(id)
-    );
-EOF
+    );"
+    psql -q -U $pg_user -d $pg_db -c "$sql"
 }
 
 cvd_fill_countries ()
@@ -50,27 +52,30 @@ cvd_fill_countries ()
     local data=$(cat "$data_file")
 
     jq_args="'to_entries | map( \
-        \"#\" + .key + \"#,\" + \
-        \"#\" + (.value.location | tostring) + \"#,\" + \
-        \"#\" + (.value.continent | tostring) + \"#,\" + \
-        \"#\" + (.value.population | tostring) + \"#\n\") | .[]'"
+        \"\$\$\" + .key + \"\$\$,\" + \
+        \"\$\$\" + (.value.location | tostring) + \"\$\$,\" + \
+        \"\$\$\" + (.value.continent | tostring) + \"\$\$,\" + \
+        \"\$\$\" + (.value.population | tostring) + \"\$\$\n\") | .[]'"
     values=$(echo "$data" | eval "jq -r $jq_args")
 
-    echo "$values" | tr '#' \" | while IFS= read -r line; do
+    echo "$values" | while IFS= read -r line; do
         if [ -n "$line" ]
         then
-            sql="insert or replace into \
+            sql="insert into \
                 country (key,name,continent,population) \
-                values ($line)"
-            echo $sql | sqlite3 $db_file
+                values ($line) \
+                on conflict(key) do update set \
+                    name=EXCLUDED.name, \
+                    continent=EXCLUDED.continent, \
+                    population=EXCLUDED.population;"
+            psql -q -U $pg_user -d $pg_db -c "$sql"
         fi
     done
 }
 
 cvd_init ()
 {
-    cvd_db_init && \
-    cvd_fill_countries
+    cvd_db_init && cvd_fill_countries
 }
 
 cvd_get ()
@@ -92,12 +97,12 @@ cvd_get ()
     local day_from=$(date_to_day $date_from)
     local day_to=$(date_to_day $date_to)
     local sql="select 
-        date (day * 86400 + strftime('%s', '$day_start'), 'unixepoch'),
+        to_char(TIMESTAMP 'epoch' + (day * 86400 + extract(epoch from timestamp '$day_start')) * INTERVAL '1 second', 'YYYY-MM-DD'),
         new_cases from data
         where day >= '$day_from'
         and day <= '$day_to'
         and country = (select id from country where name = '$country');"
-    echo "$sql" | sqlite3 "$db_file"
+    psql -q -U $pg_user -d $pg_db -c "$sql"
 }
 
 cvd_upd ()
@@ -124,18 +129,20 @@ cvd_upd ()
         | map(select(.date >= \"$date_from\" and .date <= \"$date_to\") |\
         {\"key\": \$key, \"day\": .date, \"new_cases\": .new_cases})) \
         | .[] | .[] | (\
-        \"(select (strftime(#%s#, #\" + .day + \"#) - strftime(#%s#, #$day_start#)) / 86400),\" + \
-        \"(select id from country where key = #\" + .key + \"#),\" + \
+        \"(select (extract(epoch from timestamp \$\$\" + .day + \"\$\$) - extract(epoch from timestamp \$\$$day_start\$\$)) / 86400),\" + \
+        \"(select id from country where key = \$\$\" + .key + \"\$\$),\" + \
         (.new_cases | tostring))'"
     values=$(echo "$data" | eval "jq -r $jq_args")
 
-    echo "$values" | tr '#' \" | while IFS= read -r line; do
+    echo "$values" | while IFS= read -r line; do
         if [ -n "$line" ]
         then
-            local sql="insert or replace into 
-                data (day, country, new_cases)
-                values ($line);"
-            echo $sql | sqlite3 $db_file
+            local sql="insert into \
+                data (day, country, new_cases) \
+                values ($line)
+                on conflict (day, country) do update set \
+                new_cases=EXCLUDED.new_cases;"
+            psql -q -U $pg_user -d $pg_db -c "$sql"
         fi
     done
 }
@@ -143,7 +150,7 @@ cvd_upd ()
 cvd_sql ()
 {
     local sql="$1"
-    echo "$sql" | sqlite3 $db_file
+    psql -q -U $pg_user -d $pg_db -c "$sql"
 }
 
 cvd_refresh ()
